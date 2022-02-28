f13781ac15338aa3da8e52e5bbdc9f0e8a17af90
Mẫn Hồ <72002135+HoTrinhMan@users.noreply.github.com>
2022-02-28 07:51:34 +0700

    code update salary 7 year

--- GitWorkshopProject/SalaryCalculator/Program.cs
+++ GitWorkshopProject/SalaryCalculator/Program.cs
@@ -6,6 +6,9 @@ namespace SalaryCalculator
     {
         static void Main(string[] args)
         {
+            var managerSalary = EmployeeService.CalculateSalary(Manager.Manager, 7);
+            Console.WriteLine(string.Format("Manager's Salary: {0}", managerSalary));
+
             var engineerSalary = EmployeeService.CalculateSalary(EmployeeType.Engineer, 2);
             Console.WriteLine(string.Format("Engineer's Salary: {0}", engineerSalary));
 
