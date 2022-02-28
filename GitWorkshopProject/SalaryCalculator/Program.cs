0e70bdb84d489142c3cab6f289b0cbee8e68c675
Minhtoan-18DTHQA1 <leenedquyen@gmail.com>
2022-02-28 08:25:10 +0700

    update employee, program

--- GitWorkshopProject/SalaryCalculator/Program.cs
+++ GitWorkshopProject/SalaryCalculator/Program.cs
@@ -9,6 +9,10 @@ namespace SalaryCalculator
             var engineerSalary = EmployeeService.CalculateSalary(EmployeeType.Engineer, 2);
             Console.WriteLine(string.Format("Engineer's Salary: {0}", engineerSalary));
 
+            var ManagerSalary = EmployeeService.CalculateSalary(EmployeeType.Manager, 7);
+            Console.WriteLine(string.Format("Manager's Salary: {0}", engineerSalary));
+
+
             Console.ReadLine();
         }
     }
