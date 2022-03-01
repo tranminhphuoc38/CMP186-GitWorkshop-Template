b31abb7f3c1a8944e486ea5cdf343bd3ad1ecdbd
Duc171 <72054805+Duc171@users.noreply.github.com>
2022-03-01 12:49:02 +0700

    Update Program.cs

--- GitWorkshopProject/SalaryCalculator/Program.cs
+++ GitWorkshopProject/SalaryCalculator/Program.cs
@@ -6,7 +6,7 @@ namespace SalaryCalculator
     {
         static void Main(string[] args)
         {
-            var engineerSalary = EmployeeService.CalculateSalary(EmployeeType.Engineer, 2);
+            var engineerSalary = EmployeeService.CalculateSalary(EmployeeType.Expert, 9);
             Console.WriteLine(string.Format("Engineer's Salary: {0}", engineerSalary));
 
             Console.ReadLine();
