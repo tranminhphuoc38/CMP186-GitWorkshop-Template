b29d40efdc6762205caf30338f43ef84503814f0
PeterN00 <nguyentanphuc03092000hiie@gmail.com>
2022-03-03 07:37:37 +0700

    task01

--- GitWorkshopProject/SalaryCalculator/Program.cs
+++ GitWorkshopProject/SalaryCalculator/Program.cs
@@ -6,7 +6,8 @@ namespace SalaryCalculator
     {
         static void Main(string[] args)
         {
-            var engineerSalary = EmployeeService.CalculateSalary(EmployeeType.Engineer, 2);
+            var engineerSalary = 
+                EmployeeService.CalculateSalary(EmployeeType.SeniorManager, 7);
             Console.WriteLine(string.Format("Engineer's Salary: {0}", engineerSalary));
 
             Console.ReadLine();
