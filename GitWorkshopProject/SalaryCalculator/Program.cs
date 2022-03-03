5b7ea9ceaa86eeb3c7618d04d05b12075839c6db
User Name <phankhanhduy050600hiie@gmail.com>
2022-03-03 17:51:11 +0700

    KhDuy

--- GitWorkshopProject/SalaryCalculator/Program.cs
+++ GitWorkshopProject/SalaryCalculator/Program.cs
@@ -9,6 +9,12 @@ namespace SalaryCalculator
             var engineerSalary = EmployeeService.CalculateSalary(EmployeeType.Engineer, 2);
             Console.WriteLine(string.Format("Engineer's Salary: {0}", engineerSalary));
 
+            var managerSalary = EmployeeService.CalculateSalary(EmployeeType.Manager, 7);
+            Console.WriteLine(string.Format("Engineer's Salary: {0}", managerSalary));
+
+            var expertSalary = EmployeeService.CalculateSalary(EmployeeType.Expert, 9);
+            Console.WriteLine(string.Format("Engineer's Salary: {0}", expertSalary));
+
             Console.ReadLine();
         }
     }
