8355db39cac62b1fa9750cb3c9bcde8a9b60ebfd
trananhphuong3008 <tranngocphuong300800hiie@gmail.com>
2022-03-03 13:51:55 +0700

    Add Salary Expert

--- GitWorkshopProject/SalaryCalculator/Program.cs
+++ GitWorkshopProject/SalaryCalculator/Program.cs
@@ -9,6 +9,9 @@ namespace SalaryCalculator
             var engineerSalary = EmployeeService.CalculateSalary(EmployeeType.Engineer, 2);
             Console.WriteLine(string.Format("Engineer's Salary: {0}", engineerSalary));
 
+             var expertSalary = EmployeeService.CalculateSalary(EmployeeType.Expert, 9);
+            Console.WriteLine(string.Format("Expert's Salary: {0}", expertSalary));
+
             Console.ReadLine();
         }
     }
