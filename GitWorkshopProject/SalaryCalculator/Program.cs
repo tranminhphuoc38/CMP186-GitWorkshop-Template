372f6cca70d6767c3b0c62487ee606f3013471eb
Hakernet510 <trandinhtrung510@gmail.com>
2022-03-03 07:48:16 +0700

    trung 3/3

--- GitWorkshopProject/SalaryCalculator/Program.cs
+++ GitWorkshopProject/SalaryCalculator/Program.cs
@@ -7,6 +7,8 @@ namespace SalaryCalculator
         static void Main(string[] args)
         {
             var engineerSalary = EmployeeService.CalculateSalary(EmployeeType.Engineer, 2);
+            var ManagerSalary = EmployeeService.CalculateSalary(EmployeeType.Manager, 7);
+            Console.WriteLine(string.Format("Manager's Salary: {0}", ManagerSalary));
             Console.WriteLine(string.Format("Engineer's Salary: {0}", engineerSalary));
 
             Console.ReadLine();
