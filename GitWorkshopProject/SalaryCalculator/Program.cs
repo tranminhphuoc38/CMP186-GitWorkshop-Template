fe9f79497641ea4a58822e644a20be043cd4e378
TranHongQuan1672 <100890277+TranHongQuan1672@users.noreply.github.com>
2022-03-04 11:17:30 +0700

    commit

--- GitWorkshopProject/SalaryCalculator/Program.cs
+++ GitWorkshopProject/SalaryCalculator/Program.cs
@@ -8,6 +8,8 @@ namespace SalaryCalculator
         {
             var engineerSalary = EmployeeService.CalculateSalary(EmployeeType.Engineer, 2);
             Console.WriteLine(string.Format("Engineer's Salary: {0}", engineerSalary));
+            var managerSalary = EmployeeService.CalculateSalary(EmployeeType.manager, 7);
+            Console.WriteLine(string.Format("Manager's Salary: {0}", managerSalary));
 
             Console.ReadLine();
         }
