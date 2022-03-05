4df3bbd16f87e3a5be8dc1379860a6b469e112a5
dattruong2110 <90130400+dattruong2110@users.noreply.github.com>
2022-03-05 13:26:01 +0700

    Update salary for manager with 7 year experience

--- GitWorkshopProject/SalaryCalculator/Program.cs
+++ GitWorkshopProject/SalaryCalculator/Program.cs
@@ -6,8 +6,11 @@ namespace SalaryCalculator
     {
         static void Main(string[] args)
         {
-            var engineerSalary = EmployeeService.CalculateSalary(EmployeeType.Engineer, 7);
+            var engineerSalary = EmployeeService.CalculateSalary(EmployeeType.Engineer, 2);
+            var managerSalary = EmployeeService.CalculateSalary(EmployeeType.Manager, 7);
+            
             Console.WriteLine(string.Format("Engineer's Salary: {0}", engineerSalary));
+            Console.WriteLine(string.Format("Manager's Salary: {0}", managerSalary));
 
             Console.ReadLine();
         }
