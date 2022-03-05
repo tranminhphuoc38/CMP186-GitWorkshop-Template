7d12fbf1a37aa9884be72b38516ead221db8f704
Lâm Nhất Huy <lamnhathuy120200hiie@gmail.com>
2022-03-05 20:08:08 +0700

    complete task 2

--- GitWorkshopProject/SalaryCalculator/Program.cs
+++ GitWorkshopProject/SalaryCalculator/Program.cs
@@ -7,6 +7,7 @@ namespace SalaryCalculator
         static void Main(string[] args)
         {
             var engineerSalary = EmployeeService.CalculateSalary(EmployeeType.Engineer, 2);
+            var ExpertSalary = EmployeeService.CalculateSalary(EmployeeType.Expert, 9);
             Console.WriteLine(string.Format("Engineer's Salary: {0}", engineerSalary));
 
             Console.ReadLine();
