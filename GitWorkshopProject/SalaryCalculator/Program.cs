dd8b94d57e605f6ff3806198b2598e1377a6e728
MinhKhoe <101002699+MinhKhoe@users.noreply.github.com>
2022-03-06 02:30:59 +0700

    Update Ex1

--- GitWorkshopProject/SalaryCalculator/Program.cs
+++ GitWorkshopProject/SalaryCalculator/Program.cs
@@ -7,7 +7,7 @@ namespace SalaryCalculator
         static void Main(string[] args)
         {
             var engineerSalary = EmployeeService.CalculateSalary(EmployeeService.Manager, 7);
-            Console.WriteLine(string.Format("Engineer's Salary: {30000000}", engineerSalary));
+            Console.WriteLine(string.Format("Engineer's Salary: {20000000}", engineerSalary));
 
             Console.ReadLine();
         }
